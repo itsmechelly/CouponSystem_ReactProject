61a48f903dd5285568cce0f7ad3eb15406a3b562
Chelly izraelov <hlly.lov@gmail.com>
2021-11-29 15:48:29 +0200

    ...

---
+++ src/Components/LayoutArea/Routing/CustomerRouting/CustomerRouting.tsx
@@ -0,0 +1,22 @@
+import { Switch, Route } from "react-router-dom";
+import CustomerHome from "../../../CustomerArea/CustomerHome/CustomerHome";
+import GetAllCouponsByCategory from "../../../CustomerArea/GetAllCouponsByCategory/GetAllCouponsByCategory";
+import GetAllCouponsByMaxPrice from "../../../CustomerArea/GetAllCouponsByMaxPrice/GetAllCouponsByMaxPrice";
+import GetAllCustomerCoupons from "../../../CustomerArea/GetAllCustomerCoupons/GetAllCustomerCoupons";
+import GetCustomerDetails from "../../../CustomerArea/GetCustomerDetails/GetCustomerDetails";
+
+function CustomerRouting(): JSX.Element {
+    return (
+        <div className="CustomerRouting">
+            <Switch>
+                <Route path="/customer" component={CustomerHome} exact />
+                <Route path="/customer/getAllCustomerCoupons" component={GetAllCustomerCoupons} exact />
+                <Route path="/customer/getAllCouponsByCategory" component={GetAllCouponsByCategory} exact />
+                <Route path="/customer/getAllCouponsUnderMaxPrice" component={GetAllCouponsByMaxPrice} exact />
+                <Route path="/customer/getCustomerDetails" component={GetCustomerDetails} exact />
+            </Switch>
+        </div>
+    );
+}
+
+export default CustomerRouting;
