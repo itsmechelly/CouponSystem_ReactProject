b359a7dcb854bf62bdb30a6b45e53c00a0b25608
Chelly izraelov <hlly.lov@gmail.com>
2021-11-29 15:16:21 +0200

    ...

--- src/Components/AdminArea/Company/AddCompany/AddCompany.tsx
+++ src/Components/AdminArea/Company/AddCompany/AddCompany.tsx
@@ -98,23 +98,23 @@ function AddCompany(): JSX.Element {
                         margin="normal"
                         fullWidth
                         autoComplete="current-password"
+                        inputProps={{ pattern: "/^[a-zA-Z0-9]+$/gi", minLength: 4, }}
                         {...register("password", {
                             required: { value: true, message: "Missing password." },
                             minLength: { value: 4, message: "Password too short, should be at least 4 characters." },
                             pattern: { value: /^[a-zA-Z0-9]+$/gi, message: "Password is not valid, only letters and numbers are permitted." }
                         })}
                         type={state.showPassword ? 'text' : 'password'}
-                        inputProps={{
-                            pattern: "/^[a-zA-Z0-9]+$/gi", minLength: 4,
-                            endAdornment:
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword} edge="end">
-                                        {state.showPassword ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                        }}
+                        // InputProps={{
+                        //     endAdornment:
+                        //         <InputAdornment position="end">
+                        //             <IconButton
+                        //                 aria-label="toggle password visibility"
+                        //                 onClick={handleClickShowPassword} edge="end">
+                        //                 {state.showPassword ? <Visibility /> : <VisibilityOff />}
+                        //             </IconButton>
+                        //         </InputAdornment>
+                        // }}
                         error={!!errors.password}
                         helperText={errors.password?.message}
                     />
