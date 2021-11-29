d6a7d40ba3c9a3594f5c98a29cd54d0d0e38934c
Chelly izraelov <hlly.lov@gmail.com>
2021-11-29 15:08:17 +0200

    ...

--- src/Components/AdminArea/Company/UpdateCompany/UpdateCompany.tsx
+++ src/Components/AdminArea/Company/UpdateCompany/UpdateCompany.tsx
@@ -126,13 +126,13 @@ function UpdateCompany(): JSX.Element {
                         autoFocus
                         type="email"
                         autoComplete="email"
-                        inputProps={{ pattern: "/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/", }}
+                        // inputProps={{ pattern: "/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/", }}
                         {...register("email", {
                             required: { value: true, message: "Missing email." },
-                            pattern: { value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, message: "Email is not valid." }
+                            // pattern: { value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, message: "Email is not valid." }
                         })}
                         defaultValue={company?.email}
-                        // inputProps={{ onChange: handleChange }}
+                        inputProps={{ onChange: handleChange }}
                         error={!!errors.email}
                         helperText={errors.email?.message}
                     />
@@ -143,11 +143,11 @@ function UpdateCompany(): JSX.Element {
                         margin="normal"
                         fullWidth
                         autoComplete="current-password"
-                        inputProps={{ pattern: "/^[a-zA-Z0-9]+$/gi", minLength: 4, }}
+                        // inputProps={{ pattern: "/^[a-zA-Z0-9]+$/gi", minLength: 4, }}
                         {...register("password", {
                             required: { value: true, message: "Missing password." },
-                            minLength: { value: 4, message: "Password too short, should be at least 4 characters." },
-                            pattern: { value: /^[a-zA-Z0-9]+$/gi, message: "Password is not valid, only letters and numbers are permitted." }
+                            // minLength: { value: 4, message: "Password too short, should be at least 4 characters." },
+                            // pattern: { value: /^[a-zA-Z0-9]+$/gi, message: "Password is not valid, only letters and numbers are permitted." }
                         })}
                         error={!!errors.password}
                         helperText={errors.password?.message}
