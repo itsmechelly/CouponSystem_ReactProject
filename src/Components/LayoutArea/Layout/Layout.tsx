402dc76e394b6da9a4d61f8c9c2e0610807a59af
Chelly izraelov <hlly.lov@gmail.com>
2021-11-29 09:06:55 +0200

    ...

--- src/Components/LayoutArea/Layout/Layout.tsx
+++ src/Components/LayoutArea/Layout/Layout.tsx
@@ -1,8 +1,6 @@
-import { BrowserRouter, Redirect, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AuthMenu from "../../AuthArea/AuthMenu/AuthMenu";
-import Login from "../../AuthArea/Login/Login";
 import Footer from "../Footer/Footer";
-import Main from "../Main/Main";
 import Routing from "../Routing/Routing";
 
 function Layout(): JSX.Element {
@@ -13,18 +11,7 @@ function Layout(): JSX.Element {
                     <AuthMenu />
                 </header>
                 <main>
-                    {/* <Routing /> */}
-                    <Route path="/layout" component={Main} exact />
-
-                    {/* <Route path="/admin" component={AdminMain} />
-                    <Route path="/company" component={CompanyMain} />
-                    <Route path="/customer" component={CustomerMain} /> */}
-
-                    {/* <Route path="/register" component={Register} exact /> */}
-                    <Route path="/login" component={Login} exact />
-                    {/* <Route path="/logout" component={Logout} exact /> */}
-
-                    <Redirect from="/" to="/layout" exact />
+                    <Routing />
                 </main>
                 <footer className="footer">
                     <Footer />
--- src/Components/LayoutArea/Routing/Routing.tsx
+++ src/Components/LayoutArea/Routing/Routing.tsx
@@ -12,23 +12,24 @@ function Routing(): JSX.Element {
     return (
         <div className="Routing">
 
-            {/* <Switch>
+            <Switch>
 
-                <Route path="/layout" component={Main} exact />
-
-                <Route path="/admin" component={AdminMain} />
-                <Route path="/company" component={CompanyMain} />
-                <Route path="/customer" component={CustomerMain} />
-
-                <Route path="/register" component={Register} exact />
+                {/* <Route path="/layout" component={Main} exact /> */}
                 <Route path="/login" component={Login} exact />
-                <Route path="/logout" component={Logout} exact />
 
-                <Redirect from="/" to="/layout" exact />
+                {/* <Route path="/admin" component={AdminMain} />
+                <Route path="/company" component={CompanyMain} />
+                <Route path="/customer" component={CustomerMain} /> */}
+
+                {/* <Route path="/register" component={Register} exact />
+                <Route path="/login" component={Login} exact />
+                <Route path="/logout" component={Logout} exact /> */}
+
+                <Redirect from="/" to="/login" exact />
                 
-                <Route component={Page404} />
+                {/* <Route component={Page404} /> */}
 
-            </Switch> */}
+            </Switch>
         </div>
     );
 }
