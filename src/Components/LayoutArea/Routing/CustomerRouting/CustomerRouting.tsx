a7be7b1dffe21ba2be17c90b0a29520a32999a26
Chelly izraelov <hlly.lov@gmail.com>
2021-11-29 16:27:45 +0200

    ...

--- src/Components/LayoutArea/Routing/CustomerRouting/CustomerRouting.tsx
+++ src/Components/LayoutArea/Routing/CustomerRouting/CustomerRouting.tsx
@@ -1,8 +1,8 @@
 import { Switch, Route } from "react-router-dom";
 import CustomerHome from "../../../CustomerArea/CustomerHome/CustomerHome";
-import GetAllCustomerCouponsByCategory from "../../../CustomerArea/GetAllCustomerCouponsByCategory/GetAllCustomerCouponsByCategory";
 import GetAllCouponsByMaxPrice from "../../../CustomerArea/GetAllCouponsByMaxPrice/GetAllCouponsByMaxPrice";
 import GetAllCustomerCoupons from "../../../CustomerArea/GetAllCustomerCoupons/GetAllCustomerCoupons";
+import GetCustomerCouponsByCategory from "../../../CustomerArea/GetAllCustomerCouponsByCategory/GetAllCustomerCouponsByCategory";
 import GetCustomerDetails from "../../../CustomerArea/GetCustomerDetails/GetCustomerDetails";
 
 function CustomerRouting(): JSX.Element {
@@ -11,7 +11,7 @@ function CustomerRouting(): JSX.Element {
             <Switch>
                 <Route path="/customer" component={CustomerHome} exact />
                 <Route path="/customer/getAllCustomerCoupons" component={GetAllCustomerCoupons} exact />
-                <Route path="/customer/getAllCouponsByCategory" component={GetAllCustomerCouponsByCategory} exact />
+                <Route path="/customer/getAllCouponsByCategory" component={GetCustomerCouponsByCategory} exact />
                 <Route path="/customer/getAllCouponsUnderMaxPrice" component={GetAllCouponsByMaxPrice} exact />
                 <Route path="/customer/getCustomerDetails" component={GetCustomerDetails} exact />
             </Switch>
