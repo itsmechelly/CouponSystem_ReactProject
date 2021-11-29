f5d8a5b5e8d9dcc890535e5fad134da8fd35f083
Chelly izraelov <hlly.lov@gmail.com>
2021-11-29 15:27:47 +0200

    added more admin components

--- src/Components/LayoutArea/Routing/AdminRouting/AdminRouting.tsx
+++ src/Components/LayoutArea/Routing/AdminRouting/AdminRouting.tsx
@@ -5,11 +5,11 @@ import DeleteCompany from "../../../AdminArea/Company/DeleteCompany/DeleteCompan
 import GetAllCompanies from "../../../AdminArea/Company/GetAllCompanies/GetAllCompanies";
 import GetOneCompany from "../../../AdminArea/Company/GetOneCompany/GetOneCompany";
 import UpdateCompany from "../../../AdminArea/Company/UpdateCompany/UpdateCompany";
-// import AddCustomer from "../../../AdminArea/Customer/AddCustomer/AddCustomer";
-// import DeleteCustomer from "../../../AdminArea/Customer/DeleteCustomer/DeleteCustomer";
-// import GetAllCustomers from "../../../AdminArea/Customer/GetAllCustomers/GetAllCustomers";
-// import GetOneCustomer from "../../../AdminArea/Customer/GetOneCustomer/GetOneCustomer";
-// import UpdateCustomer from "../../../AdminArea/Customer/UpdateCustomer/UpdateCustomer";
+import AddCustomer from "../../../AdminArea/Customer/AddCustomer/AddCustomer";
+import DeleteCustomer from "../../../AdminArea/Customer/DeleteCustomer/DeleteCustomer";
+import GetAllCustomers from "../../../AdminArea/Customer/GetAllCustomers/GetAllCustomers";
+import GetOneCustomer from "../../../AdminArea/Customer/GetOneCustomer/GetOneCustomer";
+import UpdateCustomer from "../../../AdminArea/Customer/UpdateCustomer/UpdateCustomer";
 
 function AdminRouting(): JSX.Element {
     return (
@@ -23,11 +23,11 @@ function AdminRouting(): JSX.Element {
                 <Route path="/admin/getOneCompany/:id" component={GetOneCompany} exact />
                 <Route path="/admin/getAllCompanies" component={GetAllCompanies} exact />
 
-                {/* <Route path="/admin/addCustomer" component={AddCustomer} exact />
-                <Route path="/admin/updateCustomer/:id" component={UpdateCustomer} exact />
+                {/* <Route path="/admin/addCustomer" component={AddCustomer} exact /> */}
+                {/* <Route path="/admin/updateCustomer/:id" component={UpdateCustomer} exact /> */}
                 <Route path="/admin/deleteCustomer" component={DeleteCustomer} exact />
                 <Route path="/admin/getOneCustomer/:id" component={GetOneCustomer} exact />
-                <Route path="/admin/getAllCustomers" component={GetAllCustomers} exact /> */}
+                <Route path="/admin/getAllCustomers" component={GetAllCustomers} exact />
             </Switch>
 
         </div>
