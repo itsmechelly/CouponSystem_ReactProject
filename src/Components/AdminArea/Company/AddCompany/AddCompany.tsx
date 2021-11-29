bb99c50b839551362dc9106b19b6de2638d5595c
Chelly izraelov <hlly.lov@gmail.com>
2021-11-29 19:44:23 +0200

    ...

--- src/Components/AdminArea/Company/AddCompany/AddCompany.tsx
+++ src/Components/AdminArea/Company/AddCompany/AddCompany.tsx
@@ -1,12 +1,23 @@
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router";
 import CouponModel from "../../../../Models/CouponModel";
+import { ClientType } from "../../../../Models/UserModel";
+import store from "../../../../Redux/Store";
+import notify from "../../../../Services/Notification";
 import "./AddCompany.css";
 
 function AddCompany(): JSX.Element {
 
     let { register, handleSubmit, formState: { errors }, getValues } = useForm<CouponModel>({ mode: "all" });
     const history = useHistory();
+
+    useEffect(() => {
+        if (store.getState().AuthState.user?.clientType !== ClientType.COMPANY) {
+            notify.error("Please log in");
+            history.push("/login");
+        }
+    }, []);
 
     return (
         <div className="AddCompany">
