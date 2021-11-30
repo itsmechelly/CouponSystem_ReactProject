5b915902629587fee7bc4c77ced5ee0b8a1f33cc
Chelly izraelov <hlly.lov@gmail.com>
2021-11-30 09:41:41 +0200

    ...

--- src/Components/AdminArea/Company/AddCompany/AddCompany.tsx
+++ src/Components/AdminArea/Company/AddCompany/AddCompany.tsx
@@ -1,29 +1,12 @@
-import { Button, ButtonGroup, IconButton, InputAdornment, TextField, Typography } from "@material-ui/core";
-import { Add, ClearAll, Send, Visibility, VisibilityOff } from "@material-ui/icons";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { useHistory } from "react-router-dom";
-import CompanyModel from "../../../../Models/CompanyModel";
-import { ClientType } from "../../../../Models/UserModel";
-import { companyAddedAction } from "../../../../Redux/CompaniesState";
-import store from "../../../../Redux/Store";
-import globals from "../../../../Services/Globals";
-import jwtAxios from "../../../../Services/jwtAxios";
-import notify from "../../../../Services/Notification";
 import "./AddCompany.css";
 
 function AddCompany(): JSX.Element {
 
     return (
         <div className="AddCompany">
-            <div className="Container">
 
-                <Add />
-                <Typography variant="h3" className="Headline">
-                    Add Company
-                </Typography>
+                <h1>BH</h1>
 
-            </div>
         </div >
     );
 }
