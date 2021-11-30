81a5c23220425e4fd1daa27cae29ce71cccf9c41
Chelly izraelov <hlly.lov@gmail.com>
2021-11-30 10:18:31 +0200

    ...

--- src/Components/AdminArea/Company/AddCompany/AddCompany.tsx
+++ src/Components/AdminArea/Company/AddCompany/AddCompany.tsx
@@ -1,3 +1,5 @@
+import { Typography } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 import "./AddCompany.css";
 
 function AddCompany(): JSX.Element {
@@ -7,6 +9,11 @@ function AddCompany(): JSX.Element {
 
                 <h1>BH</h1>
 
+                <Add />
+                <Typography variant="h3" className="Headline">
+                    Add Company
+                </Typography>
+                
         </div >
     );
 }
