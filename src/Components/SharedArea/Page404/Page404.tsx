9881a159a5d1da68e01023701232be9e21e592ad
Chelly izraelov <hlly.lov@gmail.com>
2021-11-30 17:31:08 +0200

    ...

--- src/Components/SharedArea/Page404/Page404.tsx
+++ src/Components/SharedArea/Page404/Page404.tsx
@@ -3,7 +3,6 @@ import "./Page404.css";
 function Page404(): JSX.Element {
     return (
         <div className="Page404">
-            <h1>it's not here... 404 404 404 404 404 404 404...</h1>
         </div>
     );
 }
